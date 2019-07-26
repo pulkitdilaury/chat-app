177846bd88f67bb61b30e4726b3ad24d54de39db
Pulkit Dilaury <pulkit.dilaury29@gmail.com>
2019-07-26 17:55:31 +0530

    Add form for messages and show incoming messages

--- public/js/index.js
+++ public/js/index.js
@@ -16,4 +16,26 @@ console.log('disconnected to server');
 
 socket.on('newMessage',function(message){
 console.log('newMessage',message);
+var li =jQuery('<li></li>');
+li.text(`${message.from}: ${message.text}`);
+
+jQuery('#messages').append(li);
+});
+
+
+// socket.emit('createMessage',{
+// from: "pulkit",
+// text: "Hlooo"
+// },function(data){
+// 	console.log('got it',data);
+// });
+
+jQuery('#message-form').on('submit',function(e){
+e.preventDefault();
+socket.emit('createMessage',{
+	from:'User',
+	text:jQuery('[name=message]').val()
+},function(){
+
+})
 });
