924438baf6eb9bd180edf315c427dc6a93c79d21
Pulkit Dilaury <pulkit.dilaury29@gmail.com>
2019-07-26 16:19:35 +0530

    Emit newMessage on  CreateMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,10 @@ var socket= io();
 socket.on('connect',function(){
 console.log('connected to server');
 
-socket.emit('createMessage',{
-	to: 'pulkit dilaury',
-	text:'hey,pulkit'
-});
+// socket.emit('createMessage',{
+// 	to: 'pulkit dilaury',
+// 	text:'hey,pulkit'
+// });
 
 });
 socket.on('disconnect',function(){
