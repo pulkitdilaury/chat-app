203603c00f37f8ed59a63c274747e8755200b1c1
Pulkit Dilaury <pulkit.dilaury29@gmail.com>
2019-07-27 16:11:43 +0530

    Add geolocation support via geolocation api

--- public/js/index.js
+++ public/js/index.js
@@ -22,7 +22,15 @@ li.text(`${message.from}: ${message.text}`);
 jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage',function(message){
+var li=jQuery('<li></li>');
+var a=jQuery('<a target ="_blank">My current location</a>');
 
+li.text(`${message.from}:`);
+a.attr('href',message.url);
+li.append(a);
+jQuery('#messages').append(li);
+});
 // socket.emit('createMessage',{
 // from: "pulkit",
 // text: "Hlooo"
@@ -38,4 +46,20 @@ socket.emit('createMessage',{
 },function(){
 
 })
+});
+
+var locationButton=jQuery('#send-location');
+locationButton.on('click',function(){
+	if(!navigator.geolocation){
+		return alert('Geolaction not supported by your browser');
+	}
+
+	navigator.geolocation.getCurrentPosition(function (position){
+		socket.emit('createLocationMessage',{
+			latitude: position.coords.latitude,
+			longitude: position.coords.longitude
+		});
+	},function(){
+		alert('Unable to fetch location');
+	});
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect=require('expect');
 
-var {generateMessage}=require('./message');
+var {generateMessage,generateLocationMessage}=require('./message');
 describe('generateMessage',()=>{
 it('should generate correct message object',()=>{
 var from ='pulkit';
@@ -11,4 +11,17 @@ expect(message.createdAt).toBeA('number');
 expect(message).toInclude({from,text});
 });
 
+});
+
+describe('generateLocationMessage',()=>{
+	it('should generate currect location object',()=>{
+		var from='DEB';
+		var latitude = 14;
+		var longitude =19;
+		var url='https://www.google.com/maps?=14,19';
+		var message =generateLocationMessage(from,latitude,longitude); 
+	
+	expect(message.createdAt).toBeA('number');
+expect(message).toInclude({from,url});
+	});
 });
