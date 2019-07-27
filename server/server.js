f2ae08b135eaee6af4622362a7d3d99220328b45
Pulkit Dilaury <pulkit.dilaury29@gmail.com>
2019-07-27 17:17:05 +0530

    css styles

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ socket.broadcast.emit('newMessage',generateMessage('Admin','New user joined'));
 socket.on('createMessage',(message,callback)=>{
 console.log('createMessage',message);
 io.emit('newMessage',generateMessage(message.from,message.text));
-callback('This is from server');
+callback();
 // socket.broadcast.emit('newMessage',{
 // 	from: message.from ,
 // 	text: message.text ,
@@ -36,8 +36,8 @@ callback('This is from server');
 // });
 });
 
-socket.on('createLocationMessage',(coords)=>{
-io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+socket.on('createLocationMessage',(coords) => {
+io.emit('newLocationMessage',generateLocationMessage('Admin', coords.latitude , coords.longitude));
 });
 
 socket.on('disconnect',()=>{
