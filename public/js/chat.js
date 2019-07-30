0ece237df1a0d64e24f5eea2ed24afc753f6caf4
Pulkit Dilaury <pulkit.dilaury29@gmail.com>
2019-07-30 12:10:27 +0530

    Sending messages to people in same room

--- public/js/chat.js
+++ public/js/chat.js
@@ -98,7 +98,7 @@ e.preventDefault();
 var messageTextBox=jQuery('[name=message]');
 
 socket.emit('createMessage',{
-	from:'User',
+
 	text:messageTextBox.val()
 },function(){
 messageTextBox.val('')
